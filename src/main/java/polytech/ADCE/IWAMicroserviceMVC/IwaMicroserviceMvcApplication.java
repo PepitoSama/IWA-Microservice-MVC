94ca43e08ac7369774343f4282e7caf4fe9d45fe
PepitoSama <etienne.saimond@gmail.com>
2020-11-11 16:52:39 +0100

    Show Location

---
+++ src/main/java/polytech/ADCE/IWAMicroserviceMVC/IwaMicroserviceMvcApplication.java
@@ -0,0 +1,13 @@
+package polytech.ADCE.IWAMicroserviceMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwaMicroserviceMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwaMicroserviceMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/polytech/ADCE/IWAMicroserviceMVC/IwaMicroserviceMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package polytech.ADCE.IWAMicroserviceMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwaMicroserviceMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
